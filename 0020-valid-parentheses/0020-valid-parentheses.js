58757d6fce8c981d8650d7d8fa35f2c875c48d1c
SaiF <isaif041@gmail.com>
2024-06-03 19:52:49 +0600

    Time: 58 ms (54.62%), Space: 50.2 MB (53.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -11,7 +11,7 @@ const isValid = function (s) {
     if (map[char]) {
       stack.push(char);
     } else {
-      if (stack.length === 0 || map[stack.pop()] !== char) {
+      if (map[stack.pop()] !== char) {
         return false;
       }
     }
