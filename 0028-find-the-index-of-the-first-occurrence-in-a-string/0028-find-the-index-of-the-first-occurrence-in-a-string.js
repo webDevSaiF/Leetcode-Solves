89639efc44da2c2883e168f5fd78b75a401455a6
SaiF <isaif041@gmail.com>
2024-06-07 14:13:40 +0600

    Time: 50 ms (70.02%), Space: 48.7 MB (73.33%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -5,4 +5,5 @@ const strStr = function (haystack, needle) {
   for (let i = 0; i < haystack.length; i++) {
     if (haystack.slice(i, i + needle.length) === needle) return i;
   }
+    return -1;
 };
